eb466c576f1f82680561207c7cf85c3030f58303
Daryl <morenodaryl@hotmail.com>
2015-10-21 19:57:14 -0600

    Se arregla problema de preflight

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module ForoApiRest
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
       end
     end
 
